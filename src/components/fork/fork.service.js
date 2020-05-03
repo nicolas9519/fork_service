a2f73555e84830e3a8ee65fdd4504f8d4fc5b35d
Nicolas <nicolas95pc@gmail.com>
2020-05-03 16:55:52 -0500

    Move to global scope the declaration of fork and add execArgv to be able to debug child process

--- src/components/fork/fork.service.js
+++ src/components/fork/fork.service.js
@@ -1,9 +1,9 @@
 const { fork } = require('child_process');
+const myChild = fork('src/components/fork/fork.child.js', [], { execArgv: ["--inspect-brk=0"] });
 
 class ForkService {
 
   async get(req, res) {
-    const myChild = fork('src/components/fork/fork.child.js');
     myChild.send(50);
     return res.send(true);
   }
