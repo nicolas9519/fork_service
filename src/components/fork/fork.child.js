3d66fce14e8798539180bea8d97351a28428b315
Nicolas <nicolas95pc@gmail.com>
2020-05-03 18:08:44 -0500

    create a flag to check if the process is still running

--- src/components/fork/fork.child.js
+++ src/components/fork/fork.child.js
@@ -1,8 +1,10 @@
 class ForkChild {
   async myProcess(number) {
+    process.send(true);
     for (let i = 0; i < number; i++) {
       console.log(`number ${i}`);
     }
+    process.send(false);
   }
 }
 
--- src/components/fork/fork.service.js
+++ src/components/fork/fork.service.js
@@ -1,11 +1,22 @@
 const { fork } = require('child_process');
-const myChild = fork('src/components/fork/fork.child.js', [], { execArgv: ["--inspect-brk=0"] });
 
 class ForkService {
 
+  constructor () {
+    this.myChild = fork('src/components/fork/fork.child.js', [], { execArgv: ["--inspect-brk=0"] });
+    this.runningChild = false;
+    this.myChild.on('message', this.toggleRunningChild.bind(this));
+    this.get = this.get.bind(this);
+  }
+
   async get(req, res) {
-    myChild.send(50);
+    if (this.runningChild) return res.send('Already running');
+    this.myChild.send(50);
     return res.send(true);
+  }
+
+  async toggleRunningChild(obj) {
+    this.runningChild = obj;
   }
 
 }
